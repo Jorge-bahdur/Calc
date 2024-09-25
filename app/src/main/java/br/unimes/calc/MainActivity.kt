f27e54718f74915ce1d64d6e35d4dd0be8c38f94
Jorge <jorgebahdurjr.73@gmail.com>
2024-09-24 21:39:01 -0300

    Commit Projeto Telas

---
+++ app/src/main/java/br/unimes/calc/MainActivity.kt
@@ -0,0 +1,34 @@
+package br.unimes.calc
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val edtEmail = findViewById<EditText>(R.id.edt_email)
+        val edtPass = findViewById<EditText>(R.id.edt_senha)
+        val btnEntrar = findViewById<Button>(R.id.btn_button)
+
+        btnEntrar.setOnClickListener{
+            val email = edtEmail.text.toString()
+            val pass = edtPass.text.toString().toInt()
+            if(email.equals("joao")&&pass==123){
+                val intent = Intent(this,UsuarioActivity::class.java)
+                startActivity(intent)
+                }else{
+                Toast.makeText(this, "Dados Incorretos!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
